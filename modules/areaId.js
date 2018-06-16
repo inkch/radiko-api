e589542f310fb8a3b5758df116e631996e34ca7d
tide_inokuchi <inokuchi@tidedev.jp>
2018-06-16 12:27:07 +0900

    Fix: eslint

--- modules/areaId.js
+++ modules/areaId.js
@@ -3,6 +3,6 @@ const util = require('./util.js');
 const areaId = async () => {
   const raw = await util.getXml('http://radiko.jp/area');
   return raw.match(/JP\d{2}/)[0];
-}
+};
 
 module.exports = areaId;
